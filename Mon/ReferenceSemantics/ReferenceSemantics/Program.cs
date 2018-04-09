374c81766585b9fc3fb2d5cfb033d2dc03cf4ad5
Christian Nagel <christian@christiannagel.com>
2018-04-09 14:53:16 +0200

    reference semantics

---
+++ Mon/ReferenceSemantics/ReferenceSemantics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace ReferenceSemantics
+{
+    public readonly struct MyStruct
+    {
+        public int Data => 42;
+    }
+
+    public ref struct MyStackOnlyStruct  // stack only - no boxing allowed!
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 42;
+            Foo(in a);
+
+            MyStruct ms1 = new MyStruct();
+
+            object o = ms1;  // boxing
+
+            int x = ms1.Data;
+
+
+            //ArrayList l1 = new ArrayList();
+            //l1.Add(42);
+
+            MyStackOnlyStruct ms2 = new MyStackOnlyStruct();
+            // ms2.ToString();
+
+            MagicOfSpan();
+        }
+
+        private static void MagicOfSpan()
+        {
+            int[] mydata = { 1, 2, 3, 4, 5, 6 };
+            Span<int> span1 = mydata.AsSpan();
+            Span<int> aSlice = span1.Slice(2);
+            int x1 = aSlice[0];
+        }
+
+        static void Foo(in int i)
+        {
+            Console.WriteLine(i);
+            // i = 11;
+        }
+    }
+}
